572fd24229bc25e5354ea669df06c14c3f2fa92d
Admin <Admin@STUDENT18>
2018-04-24 15:08:36 +0100

    Refactored AccountServiceDBImpl to return json strings to make testing easier. Implemented tests using Mockito to abstract away from the database.

--- src/main/java/com/qa/repository/AccountServiceDBImpl.java
+++ src/main/java/com/qa/repository/AccountServiceDBImpl.java
@@ -1,9 +1,9 @@
 package com.qa.repository;
 
-import java.util.List;
-
 import static javax.transaction.Transactional.TxType.REQUIRED;
 import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -23,34 +23,47 @@ public class AccountServiceDBImpl {
 	JSONUtil jsonUtil;
 	
 	@Transactional(REQUIRED)
-	public Account createAnAccount(Account account) {
-		em.persist(account);
-		return account;
+	public String createAnAccount(String accountAsJson) {
+		Account newAccount = jsonUtil.getObjectForJSON(accountAsJson, Account.class);
+		em.persist(newAccount);
+		return "Account has been sucessfully created";
 	}
 	
 	@Transactional(REQUIRED)
-	public Account updateAnAccount(Long id, String newAccountJSON) {
+	public String updateAnAccount(Long id, String newAccountJSON) {
 		Account oldAccount = em.find(Account.class, id);
 		Account newAccount = jsonUtil.getObjectForJSON(newAccountJSON, Account.class);
 		if (oldAccount != null) {
 			em.merge(newAccount);
 		}
-		return newAccount;
+		return "Account has been sucessfully updated";
 	}
 	
 	@Transactional(REQUIRED)
-	public Account deleteAccount(Account account) {
-		em.remove(account);
-		return account;
+	public String deleteAccount(Long accountId) {
+		Account accountToRemove = em.find(Account.class, accountId);
+		if (accountToRemove != null) {
+			em.remove(accountToRemove);
+		}
+		return "Account has been sucessfully deleted";
 	}
 	
-	public List<Account> getAllAccounts() {
-		TypedQuery<Account> query = em.createQuery("SELECT a FROM ACCOUNT a ORDER BY a.id", Account.class);
-		return query.getResultList();
+	public String getAllAccounts() {
+		TypedQuery<Account> query = em.createQuery("SELECT a FROM ACCOUNT a", Account.class);
+		List<Account> accountsList = query.getResultList();
+		return jsonUtil.getJSONForObject(accountsList);
 	}
 	
-	public Account getAnAccount(String accountNumber) {
+	public String getAnAccount(String accountNumber) {
 		TypedQuery<Account> query = em.createQuery("SELECT a FROM ACCOUNT a WHERE a.accountNumber = :accountNumber", Account.class);
-		return query.getResultList().get(0);
+		return jsonUtil.getJSONForObject(query.getResultList().get(0));
+	}
+
+	public void setEm(EntityManager em) {
+		this.em = em;
+	}
+	
+	public void setJSONUtil(JSONUtil jsonUtil) {
+		this.jsonUtil = jsonUtil;
 	}
 }
