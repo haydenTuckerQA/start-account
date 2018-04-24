4210a436a55cd07e0aeb4f17a017c1e24511d8a3
Admin <Admin@STUDENT18>
2018-04-24 16:22:55 +0100

    Added alternative Account map to use when database is unavailable

--- src/main/java/com/qa/repository/AccountServiceDBRepo.java
+++ src/main/java/com/qa/repository/AccountServiceDBRepo.java
@@ -5,6 +5,7 @@ import static javax.transaction.Transactional.TxType.SUPPORTS;
 
 import java.util.List;
 
+import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -14,14 +15,16 @@ import javax.transaction.Transactional;
 import com.qa.domain.Account;
 import com.qa.util.JSONUtil;
 
+@Default
 @Transactional(SUPPORTS)
-public class AccountServiceDBImpl {
+public class AccountServiceDBRepo implements AccountServiceRepo {
 	@PersistenceContext(unitName = "primary")
 	private EntityManager em;
 	
 	@Inject
 	JSONUtil jsonUtil;
 	
+	@Override
 	@Transactional(REQUIRED)
 	public String createAnAccount(String accountAsJson) {
 		Account newAccount = jsonUtil.getObjectForJSON(accountAsJson, Account.class);
@@ -29,6 +32,7 @@ public class AccountServiceDBImpl {
 		return "Account has been sucessfully created";
 	}
 	
+	@Override
 	@Transactional(REQUIRED)
 	public String updateAnAccount(Long id, String newAccountJSON) {
 		Account oldAccount = em.find(Account.class, id);
@@ -39,6 +43,7 @@ public class AccountServiceDBImpl {
 		return "Account has been sucessfully updated";
 	}
 	
+	@Override
 	@Transactional(REQUIRED)
 	public String deleteAccount(Long accountId) {
 		Account accountToRemove = em.find(Account.class, accountId);
@@ -48,21 +53,24 @@ public class AccountServiceDBImpl {
 		return "Account has been sucessfully deleted";
 	}
 	
+	@Override
 	public String getAllAccounts() {
 		TypedQuery<Account> query = em.createQuery("SELECT a FROM ACCOUNT a", Account.class);
 		List<Account> accountsList = query.getResultList();
 		return jsonUtil.getJSONForObject(accountsList);
 	}
 	
+	@Override
 	public String getAnAccount(String accountNumber) {
 		TypedQuery<Account> query = em.createQuery("SELECT a FROM ACCOUNT a WHERE a.accountNumber = :accountNumber", Account.class);
 		return jsonUtil.getJSONForObject(query.getResultList().get(0));
 	}
-
+	
 	public void setEm(EntityManager em) {
 		this.em = em;
 	}
 	
+	@Override
 	public void setJSONUtil(JSONUtil jsonUtil) {
 		this.jsonUtil = jsonUtil;
 	}
